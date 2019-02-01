549b2b55a9daac50016d873443c2eed20f8dd639
Beatriz Garcia <beatrizgarcia.jim@gmail.com>
2019-02-01 18:15:52 +0100

    add JSON

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import API from '../../weather'
 import './App.scss';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      Endpoint:API,
+    }
+  }
+  
   render() {
+    const {ResponseApi}=this.state;
     return (
       <div className="App">
-        Chamán
+   
       </div>
     );
   }
