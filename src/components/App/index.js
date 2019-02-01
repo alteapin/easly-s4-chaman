1e8f4c280e4cbae881bee8049e25e663cea082f5
Stephanie Guevara <stephanieguevaragullo@gmail.com>
2019-02-01 11:08:16 +0100

    creation of WeekDetail component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.scss';
+import WeekDetail from '../WeekDetail';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         Chamán
+        <WeekDetail />
       </div>
     );
   }
---
+++ src/components/WeekDetail/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class WeekDetail extends Component {
+  render() {
+    return (
+      <div className="App">
+        HOLA
+      </div>
+    );
+  }
+}
+
+export default WeekDetail;
