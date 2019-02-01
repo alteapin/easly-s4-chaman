e4d6a83f271af91f6b34c6415e03b5752d2186d5
Stephanie Guevara <stephanieguevaragullo@gmail.com>
2019-02-01 17:42:13 +0100

    created a component for week detail

--- src/components/WeekDetail/index.js
+++ src/components/WeekDetail/index.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 
-
 class WeekDetail extends Component {
   render() {
+    const tempIcon = {
+      backgroundImage: `url(${cloud})`
+    };
     return (
-      <div className="App">
-        HOLA
+      <div className='contain-week-detail'>
+       <div style={tempIcon}> </div>
+        <div className='temp-range'>
+            <p className='temp-high'>12º</p>
+            <p className='temp-low'>5º</p>
+
+        </div>
+        <h3 class='day'>Mon</h3>
       </div>
     );
   }
