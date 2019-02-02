7432268eb5a6a371b5ae6437b3ac6bfdbe2e23ae
joanajimfer <joanajimfer@gmail.com>
2019-02-02 21:25:25 +0100

    Add editor config file

--- src/components/MaxMinTemperature/index.js
+++ src/components/MaxMinTemperature/index.js
@@ -2,14 +2,14 @@ import React, { Component } from "react";
 import "./MaxMinTemperature.scss";
 
 class MaxMinTemperature extends Component {
-  render() {
-    return (
-      <div className="container__maxMinTemperature">
-        <div className="minTemperature">2</div>
-        <div className="maxTemperature">15</div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="container__maxMinTemperature">
+                <div className="minTemperature">2</div>
+                <div className="maxTemperature">15</div>
+            </div>
+        );
+    }
 }
 
 export default MaxMinTemperature;
