98a003aa43b812506e0e6caff5bf7456effae68b
Stephanie Guevara <stephanieguevaragullo@gmail.com>
2019-02-04 20:35:38 +0100

    day selected

--- src/components/WeekDetail/index.js
+++ src/components/WeekDetail/index.js
@@ -14,7 +14,6 @@ class WeekDetail extends Component {
         <WeekDay day='Thu' icon={Sun}/>
         <WeekDay day='Fri' icon={Sun}/>
         <WeekDay day='Sat' icon={Cloud}/>
-        <WeekDay day='Sun' icon={Sun}/>
         </div>
     );
   }
