d952689d978305ba5ca30b3a611b184b87e6c962
Stephanie Guevara <stephanieguevaragullo@gmail.com>
2019-02-04 18:54:07 +0100

    merge with dev

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,12 +6,9 @@ import Header from "../Header/index";
 import Footer from '../Footer';
 import WeekDetail from '../WeekDetail';
 import sun from "../../images/sundark.png";
-<<<<<<< HEAD
 import arrayQuotes from '../arrayQuotes';
-=======
 import { Switch, Route } from 'react-router-dom';
 import DailyDetail from "../DailyDetail";
->>>>>>> 03d221f1186b2653af94a56eedd38b9d55a6489f
 
 const url =
     "http://api.openweathermap.org/data/2.5/weather?APPID=e0911626bb8e9d069605aa705cac6693&id=6359304&units=metric&lang=es";
@@ -22,17 +19,11 @@ class App extends Component {
         this.state = {
             Endpoint: {},
             loaded: true,
-<<<<<<< HEAD
             error: '',
+            isVisible: true,
             quoteTxt: '',
         };
-
-=======
-            error: "",
-            isVisible: true,
-        };
         this.showInput = this.showInput.bind(this);
->>>>>>> 03d221f1186b2653af94a56eedd38b9d55a6489f
     }
 
     fetchApi() {
@@ -77,20 +68,14 @@ class App extends Component {
             return (
                 <div className="App">
                     <div style={BgImage} className="bg-image">
-<<<<<<< HEAD
-                        <Header />
-                        <Daily dataWeather={Endpoint} quote={quoteTxt} />
-                        <WeekDetail />
-=======
                         <Header
                         onClickAction={this.showInput}
                         visibility={this.state.visibility} />
-                        <Daily dataWeather={Endpoint} />
+                        <Daily dataWeather={Endpoint} quote={quoteTxt}/>
                         <Switch >
                             <Route exact path='/' render={() => <WeekDetail />} />
                             <Route path='/detail' render={() => <DailyDetail />} />
                         </Switch>
->>>>>>> 03d221f1186b2653af94a56eedd38b9d55a6489f
                         <Footer />
 
                     </div>
