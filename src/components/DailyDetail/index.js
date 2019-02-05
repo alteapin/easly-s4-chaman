db85a01f10a3a95d0f992bb61b8fdb0afd797166
joanajimfer <joanajimfer@gmail.com>
2019-02-05 20:08:31 +0100

    Run docs in master

--- src/components/DailyDetail/index.js
+++ src/components/DailyDetail/index.js
@@ -17,7 +17,7 @@ class DailyDetail extends React.Component {
     }
 
     fetchApi() {
-        fetch('http://api.openweathermap.org/data/2.5/forecast?APPID=e0911626bb8e9d069605aa705cac6693&id=6359304&units=metric&lang=es%E2%80%9C')
+        fetch('https://api.openweathermap.org/data/2.5/forecast?APPID=e0911626bb8e9d069605aa705cac6693&id=6359304&units=metric&lang=es%E2%80%9C')
             .then(res => res.json())
             .then(data =>
                 this.setState({
