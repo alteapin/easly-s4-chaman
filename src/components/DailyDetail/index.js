cc4a329317ad1990f8046f15bd0b836575407630
joanajimfer <joanajimfer@gmail.com>
2019-02-05 18:23:11 +0100

    eliminate background in div

--- src/components/DailyDetail/index.js
+++ src/components/DailyDetail/index.js
@@ -44,7 +44,7 @@ class DailyDetail extends React.Component {
         return (
             <div className='infoDaily-Detail rain-daily'>
 
-            <table className="dailyDetail__table">
+            <table className="dailyDetail__table rain-daily">
                 <HeadBoard />
                 <tbody>
                 <HourData hour={'6 - 9'} />
