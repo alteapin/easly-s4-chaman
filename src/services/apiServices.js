484d121f79c5cbd4cb39dae66e1ad56168deccbd
Stephanie Guevara <stephanieguevaragullo@gmail.com>
2019-02-07 18:19:41 +0100

    apis

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -1,12 +1,10 @@
-import React, { Component } from 'react';
+
+const forecastEnpoint = () => {
+    const urlForecast =
+        "http://api.openweathermap.org/data/2.5/forecast?APPID=e0911626bb8e9d069605aa705cac6693&id=6359304&units=metric&lang=es";
+    fetch(urlForecast)
+        .then(res => res.json())
+}
 
 
-class apiServices extends Component {
-render(){
-    return(
-        apiForecast() => {
-            fetch('')
-        }
-    )
-}
-}
+export default { forecastEnpoint };
