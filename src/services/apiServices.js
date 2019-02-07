22dcee2a0a6a760db6462dc60d2189a97bbc130f
Stephanie Guevara <stephanieguevaragullo@gmail.com>
2019-02-07 18:02:47 +0100

    apis

---
+++ src/services/apiServices.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class apiServices extends Component {
+render(){
+    return(
+        apiForecast() => {
+            fetch('')
+        }
+    )
+}
+}
