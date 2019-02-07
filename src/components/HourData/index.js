01c336cc2e41bd9cbb9a53634234717dbc609b95
joanajimfer <joanajimfer@gmail.com>
2019-02-07 17:32:00 +0100

    Add units to dailydetail and change font-size

--- src/components/HourData/index.js
+++ src/components/HourData/index.js
@@ -10,9 +10,9 @@ class HourData extends React.Component {
         return (
             <tr className="table__row">
                 <th className='HourData__hour'>{this.props.hour}</th>
-                <th>{this.getRndInteger(-5, 45)}</th>
-                <th>{this.getRndInteger(-5, 45)}</th>
-                <th>{this.getRndInteger(-5, 45)}</th>
+                <th>{this.getRndInteger(-5, 45)}º</th>
+                <th>{this.getRndInteger(-5, 45)} <span className='dailyDetail_units'>mm</span></th>
+                <th>{this.getRndInteger(-5, 45)} <span className='dailyDetail_units'>km/h</span></th>
             </tr>
         )
     }
