d68c74fdeb1bf800e633cece77cec6808bc8f90c
alteapin <alteap18@gmail.com>
2019-02-07 13:54:16 +0100

    first input style

--- src/components/CityLocation/index.js
+++ src/components/CityLocation/index.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import './CityLocation.scss';
+import compass from '../../icons/compass.png';
 
 class CityLocation extends Component {
 
     render() {
         return (
             <div className="citylocation">
-                <h2 className="citylocation-name">Madrid</h2>
+                <img
+                className="location-icon"
+                src={compass}
+                alt="compass"
+                />
+                <form>
+                    <label name="location">
+                        <input className="location-finder_input"
+                        type="text" placeholder="Madrid" name="location" />
+                    </label>
+                </form>
             </div>
         );
     }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,20 @@
 import React, { Component } from 'react';
 import './Header.scss';
-import LocationFinder from '../LocationFinder/index';
 import HourHeader from '../HourHeader/index';
 import CityLocation from '../CityLocation/index';
 import DayNameNumber from '../DayNameNumber';
 
 class Header extends Component {
     render() {
-    const {onClickAction, visibility, date} = this.props;
+    const {date} = this.props;
     console.log('header', this.props);
         return (
 
             <div className="header-main">
-                <LocationFinder
-                onClickAction = {onClickAction}
-                visibility = {visibility}/>
+                 <CityLocation />
                 <HourHeader/>
                 <DayNameNumber date={date} />
-                <CityLocation />
+
             </div>
         );
     }
