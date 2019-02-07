b438734f9ad3426236987ef633a682692bc5480d
alteapin <alteap18@gmail.com>
2019-02-07 11:45:09 +0100

    first change landscape version

--- src/components/CurrentHour/index.js
+++ src/components/CurrentHour/index.js
@@ -1,16 +1,16 @@
-// import React, { Component } from 'react';
-// import './CurrentHour.scss';
-// import Clock from 'react-live-clock';
+import React, { Component } from 'react';
+import './CurrentHour.scss';
+import Clock from 'react-live-clock';
 
 
-// class CurrentHour extends Component {
-//     render() {
-//         return (
-//             <div>
-//                 <Clock format="HH:mm" ticking={true} interval={1000} />
-//             </div>
-//         );
-//     }
-// }
+class CurrentHour extends Component {
+    render() {
+        return (
+            <div>
+                <Clock format="HH:mm" ticking={true} interval={1000} />
+            </div>
+        );
+    }
+}
 
-// export default CurrentHour;
+export default CurrentHour;
