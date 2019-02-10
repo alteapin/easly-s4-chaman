079daf9fdb8a077c6ac24fbd226cdcf379023ec5
joanajimfer <joanajimfer@gmail.com>
2019-02-10 12:59:05 +0100

    Rounded wind number with one decimal

--- src/components/HourData/index.js
+++ src/components/HourData/index.js
@@ -9,12 +9,14 @@ class HourData extends React.Component {
     render() {
         const {temp} = this.props;
         const roundTemp = Math.round(temp);
+        const {wind} = this.props;
+        const roundWind = wind.toFixed(1);
         return (
             <tr className="table__row">
                 <th className='HourData__hour'>{this.props.hour}</th>
                 <th>{roundTemp}º</th>
                 <th>{this.props.rain} <span className='dailyDetail_units'>mm</span></th>
-                <th>{this.props.wind} <span className='dailyDetail_units'>km/h</span></th>
+                <th>{roundWind} <span className='dailyDetail_units'>km/h</span></th>
             </tr>
         )
     }
