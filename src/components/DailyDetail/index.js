9ff06307659a5d4a2d2e37909c0622bfb3ad95ab
joanajimfer <joanajimfer@gmail.com>
2019-02-10 12:00:23 +0100

    Map the date in dailyDetail

--- src/components/DailyDetail/index.js
+++ src/components/DailyDetail/index.js
@@ -11,20 +11,26 @@ class DailyDetail extends React.Component {
     render() {
         const {todayInfo} = this.props;
         console.log('daily props', todayInfo);
+        if(todayInfo.length === 0) {
+            return(
+                <p>Buscando resultados</p>
+            )
+        } else {
         return (
             <div className='infoDaily-Detail snow-daily'>
                 <h3 className='title-detail'>Detailed information:<span className='table_title-day'> 6 Feb</span></h3>
                 <table className="dailyDetail__table">
                     <HeadBoard />
                     <tbody>
-                        <HourData hour={'6 - 9'} />
-                        <HourData hour={'9 - 12'} />
-                        <HourData hour={'12 - 15'} />
-                        <HourData hour={'15 - 18'} />
-                        <HourData hour={'18 - 21'} />
-                        <HourData hour={'21 - 00'} />
-                        <HourData hour={'00 - 3'} />
-                        <HourData hour={'3 - 6'} />
+                         {todayInfo.map(result => {
+
+                            return(
+                                <h1>{result.dt_txt}</h1>
+                            )
+
+
+                        })}
+
                     </tbody>
                 </table>
             </div>
@@ -32,7 +38,11 @@ class DailyDetail extends React.Component {
 
 
         )
-    }
+    }}
 }
 
 export default DailyDetail;
+
+
+
+
