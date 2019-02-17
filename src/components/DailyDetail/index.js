17786776fbac68c39af76407f9067db55c8d922f
joanajimfer <joanajimfer@gmail.com>
2019-02-17 18:22:19 +0100

    Changes with wind arrow

--- src/components/DailyDetail/index.js
+++ src/components/DailyDetail/index.js
@@ -32,7 +32,7 @@ class DailyDetail extends React.Component {
                          {todayInfo.map((result, key) => {
 
                             return(
-                               <HourData key={key} hour={result.dt_txt} wind={result.wind.speed}
+                               <HourData key={key} hour={result.dt_txt} wind={result.wind.speed} windDirection={result.wind.deg}
                                temp={result.main.temp} rain={result.weather[0].description}
                                rainPercent={result.rain||0}/>
                             )
--- src/components/HourData/index.js
+++ src/components/HourData/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './HourData.scss';
+import Arrow from '../../icons/right-arrow.png';
 
 class HourData extends React.Component {
 
@@ -8,11 +9,12 @@ class HourData extends React.Component {
     render() {
         const {rainPercent} = this.props;
         const rainNumber = parseInt(rainPercent['3h']);
-
         const {temp} = this.props;
         const roundTemp = Math.round(temp);
         const {wind} = this.props;
         const roundWind = wind.toFixed(1);
+        const {windDirection} = this.props;
+        const windStyle = { transform: `rotate(${windDirection}deg)` };
         const {hour} = this.props;
         const onlyTime = hour.split (' ');
         const onlyTimewithoutseconds = onlyTime[1].split(':');
@@ -21,7 +23,10 @@ class HourData extends React.Component {
                 <th className='HourData__hour'>{onlyTimewithoutseconds[0] + ':' + onlyTimewithoutseconds[1]}</th>
                 <th className='temp'>{roundTemp}º</th>
                 <th> {rainNumber||0} <span className='dailyDetail_units'>mm</span> </th>
-                <th>{roundWind} <span className='dailyDetail_units'>km/h</span></th>
+                <th>
+                <img src={Arrow} alt='wind direction arrow' style={windStyle} className='wind__arrow' />
+                {roundWind} <span className='dailyDetail_units'>km/h</span>
+                </th>
             </tr>
         )
     }
