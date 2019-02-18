5a5d6071778b9a2c64d689a66a00f1653d4f4bfd
Stephanie Guevara <stephanieguevaragullo@gmail.com>
2019-02-18 18:59:00 +0100

    docs

---
+++ docs/precache-manifest.df29ab2292a7adbdd2e3411dbdb15af3.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf589f9b09ff4098f292c050f644d20c",
+    "url": "./static/media/wind.bf589f9b.svg"
+  },
+  {
+    "revision": "437ce65f102bd7abc52d",
+    "url": "./static/css/main.d057dcf3.chunk.css"
+  },
+  {
+    "revision": "7fbc1c202f5760caea3f",
+    "url": "./static/js/1.7fbc1c20.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "0b564c8a1016b287748995bb177992ab",
+    "url": "./static/media/compass.0b564c8a.png"
+  },
+  {
+    "revision": "0556492ee6a6ff02ae8da1ef972fddfa",
+    "url": "./static/media/accenture.0556492e.png"
+  },
+  {
+    "revision": "0746d3b961bddb69f63e728a835eb2a0",
+    "url": "./static/media/whiteWithColor.0746d3b9.png"
+  },
+  {
+    "revision": "ce11979a643d383687c5f2b1a6c6a658",
+    "url": "./static/media/cloud.ce11979a.svg"
+  },
+  {
+    "revision": "d2f64c0bc144870118292891fd2fa2e2",
+    "url": "./static/media/snow.d2f64c0b.svg"
+  },
+  {
+    "revision": "607ad0a3bcee2d96d97590229370ae39",
+    "url": "./static/media/sun.607ad0a3.svg"
+  },
+  {
+    "revision": "d7f4ba85a15be2c74e101b3c6ea56f9c",
+    "url": "./static/media/rain.d7f4ba85.svg"
+  },
+  {
+    "revision": "437ce65f102bd7abc52d",
+    "url": "./static/js/main.437ce65f.chunk.js"
+  },
+  {
+    "revision": "5a6bfd2b47101b00d85cc5df4a81f676",
+    "url": "./static/media/thermometer.5a6bfd2b.svg"
+  },
+  {
+    "revision": "0d7cf7621eba6c3fb0c229f5b35145ef",
+    "url": "./static/media/cloudIcon.0d7cf762.svg"
+  },
+  {
+    "revision": "67d787a031dc3ea5e3d9979bf7f0fb90",
+    "url": "./static/media/time.67d787a0.svg"
+  },
+  {
+    "revision": "a1309ba2308de5fd77e66084fa429a27",
+    "url": "./static/media/sun.a1309ba2.png"
+  },
+  {
+    "revision": "a46c62eb985184824430681fc44992ef",
+    "url": "./static/media/night.a46c62eb.png"
+  },
+  {
+    "revision": "66be4bc3dfea04ef05fc4824bd38c39e",
+    "url": "./static/media/snow.66be4bc3.png"
+  },
+  {
+    "revision": "102d717ae3509bbade8b9348c3b6e6dd",
+    "url": "./static/media/rain.102d717a.png"
+  },
+  {
+    "revision": "cc8bcdee8c35279539bbb7228e4f8f31",
+    "url": "./static/media/cold.cc8bcdee.png"
+  },
+  {
+    "revision": "2695051285d27df03c83dac6d8d31941",
+    "url": "./static/media/logoChamanColor.26950512.png"
+  },
+  {
+    "revision": "a4f2cb80ff2ae2772e80bf30e9d78d4c",
+    "url": "./static/media/loading.a4f2cb80.gif"
+  },
+  {
+    "revision": "322bc5a76c11dca3ee96985844dcf819",
+    "url": "./index.html"
+  }
+];
